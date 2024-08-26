e9f06500b8c3ad8da99c563026580e02ad934932
geungjungsu <zkxdmswjdtn@naver.com>
2024-08-26 23:45:54 +0900

    [level 2] Title: 루시와 엘라 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
+++ 프로그래머스/2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
@@ -10,18 +10,19 @@
 # 동물의 아이디, 이름, 성별 및 중성화 여부
 # ORDER BY ANIMAL_ID
 
+# SELECT ANIMAL_ID, NAME, SEX_UPON_INTAKE
+# FROM ANIMAL_INS
+# WHERE ((NAME = 'Lucy') OR
+#       (NAME = 'Ella') OR
+#       (NAME = 'Pickle') OR
+#       (NAME = 'Rogan') OR
+#       (NAME = 'Sabrina') OR
+#       (NAME = 'Mitty'))
+# ORDER BY ANIMAL_ID
+# ;
+
 SELECT ANIMAL_ID, NAME, SEX_UPON_INTAKE
 FROM ANIMAL_INS
-WHERE ((NAME = 'Lucy') OR
-      (NAME = 'Ella') OR
-      (NAME = 'Pickle') OR
-      (NAME = 'Rogan') OR
-      (NAME = 'Sabrina') OR
-      (NAME = 'Mitty'))
+WHERE NAME IN ('Lucy', 'Ella', 'Pickle', 'Rogan', 'Sabrina', 'Mitty')
 ORDER BY ANIMAL_ID
 ;
-
-
-# SELECT 
-# FROM ANIMAL_INS
-# WHERE GROUP_CONCAT(NAME) LIKE 
